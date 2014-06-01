6ae82ac7296e85e9a018f4f40f987ea2ef61fcd9
ayaseya <ayaseya@ayaseya-PC>
2014-06-01 22:45:43 +0900

    フラグメントでログイン画面から会員情報の画面遷移を作成

--- src/com/example/cmssample/CmsLoginActivity.java
+++ src/com/example/cmssample/CmsLoginActivity.java
@@ -1,15 +1,16 @@
 package com.example.cmssample;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CmsLoginActivity extends Activity {
 
@@ -49,6 +50,11 @@ public class CmsLoginActivity extends Activity {
 	 */
 	public static class PlaceholderFragment extends Fragment {
 
+		private String member;// 会員NO
+		private EditText memberET;
+		private String password;// パスワード
+		private EditText passwordET;
+
 		public PlaceholderFragment() {
 		}
 
@@ -57,6 +63,45 @@ public class CmsLoginActivity extends Activity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_cms_login,
 					container, false);
+
+			// 会員NO、パスワードのインスタンスを取得します。
+			memberET = (EditText) rootView.findViewById(R.id.memberET);
+			passwordET = (EditText) rootView.findViewById(R.id.passwordET);
+
+			// ログインボタンのリスナーを登録します。
+			rootView.findViewById(R.id.loginBtn).setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					
+					member = memberET.getText().toString();// 入力された会員NOの文字列を取得します。
+					password = passwordET.getText().toString();// 入力されたパスワードの文字列を取得します。
+					
+					Toast.makeText(getActivity(), member, Toast.LENGTH_LONG).show();
+
+					getFragmentManager().beginTransaction()
+							.replace(R.id.container, new MemberFragment())
+							.addToBackStack(null)// バックキーを押下すると一つ前のフラグメントに戻ります。
+							.commit();
+
+				}
+			});
+
+			return rootView;
+		}
+	}
+
+	public static class MemberFragment extends Fragment {
+
+		public MemberFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_cms_member,
+					container, false);
+
 			return rootView;
 		}
 	}
